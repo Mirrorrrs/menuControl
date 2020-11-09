96b5f4a87a61f721a7a18955bfc636bff210b560
belchushka <savvashu@gmail.com>
2020-11-09 22:19:19 +0300

    100% prod (not sure)

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,7 @@ Route::middleware("auth")->group(function (){
 });
 
 
-
-Auth::routes();
-
+Route::get('login', [\App\Http\Controllers\Auth\LoginController::class,"showLoginForm"])->name('login');
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class,"login"]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get("/logout",[\App\Http\Controllers\Auth\LoginController::class,"logout"])->name("logout");
